5c08e55decb3d0b3ca10e500d0d593972c26fb1a
maicallatrace <maicallatrace@gmail.com>
2022-03-25 19:56:39 -0600

    REPO

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (x,y) => {
+    return 2000 / 100;
+  }
+  console.log(2000/100)
+
+  const square = (x) => {
+    return x * x;
+  }
+
+  const add = (x,y) => {
+    return x +y;
+  }
